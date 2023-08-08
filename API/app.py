a382b02d5264480d42d19a06488058115ba1a30a
vinsky001 <ekibet544@gmail.com>
2023-08-08 17:01:28 +0300

    HTTP request that routes /todo/api/v1.0/tasks

--- API/app.py
+++ API/app.py
@@ -1,11 +1,25 @@
 # Lets create a flask web application
-from flask import Flask
+from flask import Flask , jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return 'Learning API in depth'
+
+
+tasks = [
+    {
+        'id': 1,
+        'title':'100 days of code',
+        'description': 'Python',
+        'done': False,
+    }
+]
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_task():
+    return jsonify({'tasks': tasks})
 
 if __name__ == '__main__':
     app.run(debug=True)
