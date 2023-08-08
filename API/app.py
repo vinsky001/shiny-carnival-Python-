c0a5dc53ca18d652e6c33b8c425b4f0f3b14bff1
vinsky001 <ekibet544@gmail.com>
2023-08-08 22:29:37 +0300

    Handled Errors in JSON

--- API/app.py
+++ API/app.py
@@ -1,5 +1,5 @@
 # Lets create a flask web application
-from flask import Flask , jsonify
+from flask import Flask , jsonify, abort, make_response
 
 app = Flask(__name__)
 
@@ -30,10 +30,17 @@ which according to the HTTP specification means "Resource Not Found", which is e
  """
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_specific_task(task_id):
-    tasks = [task for task in tasks if task['id'] == task_id]
+    task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
-        return jsonify({'task': task[0]})
+    return jsonify({'task': task[0]})
+
+
+app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+# 
     
 
 if __name__ == '__main__':
